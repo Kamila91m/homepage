1580292c0eabbc94fbe0f791866916ff38fcb9e1
Kamila Mikucka <mikucka.kamila@gmail.com>
2023-06-20 10:29:12 +0200

    added functions to script.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,50 @@
-function welcome() {
-  console.log("Hello! How you doin?");
+{
+  const welcome = () => {
+    console.log("Hello! How you doin?");
+  };
+
+  const onDeleteNameClick = () => {
+    const headingNameElement = document.querySelector(".js-headingName");
+    headingNameElement.remove();
+  };
+
+  const deleteName = () => {
+    const deleteNameButtonElement = document.querySelector(
+      ".js-deleteNameButton"
+    );
+    deleteNameButtonElement.addEventListener("click", onDeleteNameClick);
+  };
+
+  const onChangeColorClick = () => {
+    const headingAboutElement = document.querySelector(".js-headingAbout");
+    const colorName = document.querySelector(".colorName");
+    headingAboutElement.classList.toggle("changeColor");
+    colorName.innerText = headingAboutElement.classList.contains("changeColor")
+      ? "czarny"
+      : "czerwony";
+  };
+
+  const changeColor = () => {
+    const changeColorButtonElement = document.querySelector(
+      ".js-buttonChangeColor"
+    );
+    changeColorButtonElement.addEventListener("click", onChangeColorClick);
+  };
+
+  const onClickRemoveText = () => {
+    const containerTextElement = document.querySelector(".js-containerText");
+    containerTextElement.remove();
+  };
+
+  const removeText = () => {
+    const buttonRemoveTextElement = document.querySelector(
+      ".js-buttonRemoveText"
+    );
+    buttonRemoveTextElement.addEventListener("click", onClickRemoveText);
+  };
+
+  welcome();
+  deleteName();
+  changeColor();
+  removeText();
 }
-
-welcome();
-
-let deleteNameButtonElement = document.querySelector(".js-deleteNameButton");
-let headingNameElement = document.querySelector(".js-headingName");
-
-deleteNameButtonElement.addEventListener("click", () => {
-  headingNameElement.remove();
-});
-
-let buttonTwo = document.querySelector(".js-buttonChangeColor");
-let headingTwo = document.querySelector(".js-headingAbout");
-let colorName = document.querySelector(".colorName");
-
-buttonTwo.addEventListener("click", () => {
-  headingTwo.classList.toggle("changeColor");
-
-  colorName.innerText = headingTwo.classList.contains("changeColor")
-    ? "czarny"
-    : "czerwony";
-});
-
-let buttonRemoveTextElement = document.querySelector(".js-buttonRemoveText");
-let containerTextElement = document.querySelector(".js-containerText");
-
-buttonRemoveTextElement.addEventListener("click", () => {
-  containerTextElement.remove();
-});
